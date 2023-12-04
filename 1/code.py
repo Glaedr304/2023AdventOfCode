ca304c7dcc87e1eca3a7479733c6d56997eb445f
Glaedr304 <Davidfrey304@gmail.com>
2023-12-03 21:02:29 -0600

    Update code.py

--- 1/code.py
+++ 1/code.py
@@ -17,15 +17,9 @@ textNumbers = {
 
 wordString = ""
 
-# print(list(textNumbers.keys())[-1])
-
 for x in textNumbers.keys():
     wordString += x
-    # print(len(textNumbers))
-    # print(textNumbers[9])
-    # print(x)
     if x != list(textNumbers.keys())[-1]:
-        # print(x)
         wordString += "|"
 
 f = open(input, "r")
@@ -35,33 +29,21 @@ def getLineNumber(line):
     characters = ""
     for character in line:
         characters += character
-        # print("characters: ", characters)
         if character.isnumeric():
             number = character
-            print("number: ", number)
             break
         elif re.search(wordString, characters) != None:
             Match = re.search(wordString, characters)
-            print("match: ", str(Match.string[Match.start():Match.end()]))
-            print("Dict Value: ", textNumbers[str(Match.string[Match.start():Match.end()])])
-            # number = str(textNumbers[Match.string[Match.start():Match.end()]])
             number = str(textNumbers[str(Match.string[Match.start():Match.end()])])
             break
         elif re.search(wordString[::-1], characters) != None:
             Match = re.search(wordString[::-1], characters)
-            print("Match String: ", Match.string)
-            # print("reverse match: ", str(Match.string)[::-1])
-            print("reverse match: ", str(Match.string[Match.start():Match.end()])[::-1])
-            print("Dict Reverse Value: ", textNumbers[str(Match.string[Match.start():Match.end()])[::-1]])
-            # number = str(textNumbers[str(Match.string[Match.start():Match.end():-1]])
             number = str(textNumbers[str(Match.string[Match.start():Match.end()])[::-1]])
             break
-
     return number
 
 for line in f:
     line = line.strip()
-    print("line: ", line)
     number = getLineNumber(line) + getLineNumber(line[::-1])
     sum += int(number)
 print(sum)
