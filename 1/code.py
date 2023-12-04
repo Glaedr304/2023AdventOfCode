072d8ba9635a6cd75a413125f764e174c2369c86
Glaedr304 <Davidfrey304@gmail.com>
2023-12-03 20:53:01 -0600

    Update code.py

--- 1/code.py
+++ 1/code.py
@@ -1,19 +1,34 @@
 input = "input.txt"
 sum = 0
+# print(list(textNumbers.keys())[-1])
+
+    # print(len(textNumbers))
+    # print(textNumbers[9])
+    # print(x)
+        # print(x)
+
+# print(wordString)
 
 f = open(input, "r")
 
 def getLineNumber(line):
     number = None
     for character in line:
+        # print("characters: ", characters)
         if character.isnumeric():
             number = character
             break
+            print("match: ", str(Match.string[Match.start():Match.end()]))
+            print("Dict Value: ", textNumbers[str(Match.string[Match.start():Match.end()])])
+            # number = str(textNumbers[Match.string[Match.start():Match.end()]])
+            print("Match String: ", Match.string)
+            # print("reverse match: ", str(Match.string)[::-1])
+            print("reverse match: ", str(Match.string[Match.start():Match.end()])[::-1])
+            print("Dict Reverse Value: ", textNumbers[str(Match.string[Match.start():Match.end()])[::-1]])
+            # number = str(textNumbers[str(Match.string[Match.start():Match.end():-1]])
     return number
 
 for line in f:
     number = getLineNumber(line) + getLineNumber(line[::-1])
     sum += int(number)
 print(sum)
-
-# this comment is to prove a point
