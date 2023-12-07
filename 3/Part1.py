fc90616b7b7ac5040d148c292a91725e5e03c387
Glaedr304 <Davidfrey304@gmail.com>
2023-12-06 21:39:03 -0600

    day 3 part 1

---
+++ 3/Part1.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+input = "input.txt"
+
+PartNumber = 0
+
+f = open(input, "r")
+
+AllCharacters = set() # readying the set
+
+Numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+NotCharacters = Numbers.union({".", "\n"})
+
+Characters = set() # AllCharacters - NotCharacters
+
+lines = list() # to be later converted to a touple
+
+for line in f:
+    characters = tuple()
+    for character in line:
+        characters += tuple(character)
+        if character in AllCharacters:
+            pass
+        else:
+            AllCharacters.add(character)
+    lines.append(characters)
+lines = tuple(lines)
+arr = np.transpose(np.array(lines))
+
+Characters = AllCharacters
+for character in NotCharacters:
+    try:
+         Characters.remove(character)
+    except:
+        pass
+
+def getNextCharacters(x, y):
+    if arr[x + 1][y].isnumeric():
+        return str(arr[x][y]) + getNextCharacters(x + 1, y)
+    else:
+        return str(arr[x][y])
+
+def checkAroundNumber(x, y, numLength):
+
+    xmin = x - 1
+    xmax = x + numLength + 1
+    ymin = y - 1
+    ymax = y + 1
+    if xmin < 0:
+        xmin += 1
+    if xmax >= arr.shape[0]:
+        xmax -= 1
+    if ymin < 0:
+        ymin += 1
+    if ymax >= arr.shape[1]:
+        ymax -= 1
+
+    for y2 in range(ymin, ymax+1):
+        for x2 in range(xmin, xmax):
+            if str(arr[x2][y2]) in Characters:
+                return True
+    return False
+
+for y in range(arr.shape[1]):
+    skipNumber = 0
+    for x in range(arr.shape[0]):
+        if skipNumber > 0:
+            skipNumber -= 1
+            continue
+        if arr[x][y].isnumeric():
+            Number = getNextCharacters(x, y)
+            if checkAroundNumber(x, y, len(Number)):
+                PartNumber += int(Number)
+            skipNumber = len(Number) - 1
+
+print("Part Number: ", PartNumber)
