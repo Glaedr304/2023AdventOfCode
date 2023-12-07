ddd40734671e642f93b7107b95a6daa4d30cbdf0
Glaedr304 <Davidfrey304@gmail.com>
2023-12-06 21:42:58 -0600

    Update part 2.py

--- 2/part 2.py
+++ 2/part 2.py
@@ -11,24 +11,38 @@ f = open(input, "r")
 
 def nextSpace( String ):
     finder = String.find(" ")
+    # print("NextSpace: ", String[:String.find(" ")].strip())
+    # print("finder: ", finder)
     if finder > 0:
         return String[:finder].strip()
     elif finder <= 0:
         return String
 
 for line in f:
+    currentMax = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    print("Line: ", line)
     game = line[line.find(" ") + 1:line.find(":")]
+    print("Game: ", game)
     line = line[line.find(": ") + 2:].replace(",", "").replace(";", "").strip()
+    # print("next space: ", nextSpace(line))
     while line != "":
         result = nextSpace(line)
+        print("Result: ", result)
         if result.isnumeric():
             blocks = int(result)
             line = line[line.find(" "):].strip()
+            print("broken? ", line)
         elif result == "":
+            print("elif: ", result == "")
             break
         else:
             color = result
             line = line.replace(color, "", 1).strip()
+            print(line)
             if blocks > currentMax[color]:
                 currentMax[color] = blocks
 
