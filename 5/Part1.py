23d22d1e11e0944000fd40e185b150c1a2cc1914
Glaedr304 <Davidfrey304@gmail.com>
2023-12-10 10:35:32 -0600

    Day 5 boilerplate

---
+++ 5/Part1.py
@@ -0,0 +1,16 @@
+import datetime
+input = "input.txt"
+
+f = open(input, "r")
+
+output = 0
+
+for line in f:
+    pass
+    
+start = datetime.datetime.now()
+end = datetime.datetime.now() - start
+print("Time: ", end)
+
+print("Output: ", output)
+
