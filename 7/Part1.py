1c54109e70c38b4879651e2d8796cd0a0a214321
Glaedr304 <Davidfrey304@gmail.com>
2023-12-12 19:48:31 -0600

    day 7 part 1 data composed

--- 7/Part1.py
+++ 7/Part1.py
@@ -1,14 +1,33 @@
 import datetime
+from pprint import pprint
 input = "input.txt"
+
+camelCards = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+winningHands = ("11111", "2111", "221", "311", "32", "41", "5")
 
 f = open(input, "r")
 
 output = 0
 
+hands = list()
+
 for line in f:
-    pass
+    hands.append({str(line[:line.find(" ")]): None, "bid": int(line[line.find(" ") + 1:line.find("\n")])})
+
+for listItem in hands:
+    thisHand = list(listItem.keys())[0]
+    listItem[thisHand] = {}
+    for char in thisHand:
+        if char in listItem[thisHand].keys():
+            listItem[thisHand][char] += 1
+        else:
+            listItem[thisHand][char] = 1
+pprint(hands)
 
 start = datetime.datetime.now()
+
+
+
 end = datetime.datetime.now() - start
 print("Time: ", end)
 
