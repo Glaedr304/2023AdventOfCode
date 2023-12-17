e8c29744f472c0e7726cf7d6a9092f152ac58c6e
Glaedr304 <Davidfrey304@gmail.com>
2023-12-16 18:50:03 -0600

    Completion of Day 9 Part 1

---
+++ 9/Part1.py
@@ -0,0 +1,55 @@
+import datetime
+
+inputFile = "input.txt"
+
+f = open(inputFile, "r")
+
+output = 0
+
+rows = f.read().splitlines()
+
+for lineIndex in range(len(rows)):
+    rows[lineIndex] = rows[lineIndex].split()
+
+for rowIndex in range(len(rows)):
+    for elementIndex in range(len(rows[rowIndex])):
+        rows[rowIndex][elementIndex] = int(rows[rowIndex][elementIndex])
+
+def isListZeros(myList):
+    for item in myList:
+        if item != 0:
+            return True
+    return False
+
+def Triangulate(thisRow): # expandTriangles would also be an appropriate name
+    differenceList = list()
+    for index in range(1, len(thisRow)):
+        differenceList.append(thisRow[index] - thisRow[index - 1])
+    return differenceList
+
+trianglesList = list()
+
+start = datetime.datetime.now()
+
+for pyramidBaseList in rows:
+    thisPyramid = list()
+    thisPyramid.append(pyramidBaseList)
+
+    while isListZeros(thisPyramid[-1]):
+        thisPyramid.append(Triangulate(thisPyramid[-1]))
+
+    trianglesList.append(thisPyramid)
+
+for pyramid in trianglesList:
+    lastRowValue = 0
+    for layerIndex in reversed(range(len(pyramid))):
+        if not isListZeros(pyramid[layerIndex]):
+            continue
+        lastRowValue += pyramid[layerIndex][-1]
+    output += lastRowValue
+
+end = datetime.datetime.now() - start
+
+print("Time: ", end)
+
+print("Output: ", output)
