07611f5e04b301fdbff32d4906ff1bc26f3660eb
Glaedr304 <Davidfrey304@gmail.com>
2023-12-19 22:27:16 -0600

    Day 11 boillerplate

---
+++ 11/Part1.py
@@ -0,0 +1,16 @@
+import datetime
+
+inputFile = "input.txt"
+
+f = open(inputFile, "r")
+
+output = 0
+
+for line in f:
+    pass
+
+start = datetime.datetime.now()
+end = datetime.datetime.now() - start
+print("Time: ", end)
+
+print("Output: ", output)
