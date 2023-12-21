34225abe29b351a84072017b9648b6c33d3443ce
Glaedr304 <Davidfrey304@gmail.com>
2023-12-21 16:34:11 -0600

    day 12 research

--- 12/Part1.py
+++ 12/Part1.py
@@ -1,13 +1,21 @@
 import datetime
+from pprint import pprint
 
 inputFile = "input.txt"
 
-f = open(inputFile, "r")
-
 output = 0
 
-for line in f:
-    pass
+processed_input = []
+
+with open(inputFile, 'r') as file:
+    input_data = file.readlines()    
+    for row in input_data:
+        springs, group_str = row.strip().split()
+        groups = list(map(int, group_str.split(',')))
+        processed_input.append((springs, groups))
+
+pprint(processed_input)
+
 
 start = datetime.datetime.now()
 end = datetime.datetime.now() - start
