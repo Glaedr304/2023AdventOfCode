e75ba4d79805c4b3d777fe96633c44e88f368141
Glaedr304 <Davidfrey304@gmail.com>
2024-03-26 13:39:54 -0500

    day 13 part 1 progress

--- 13/Part1.py
+++ 13/Part1.py
@@ -7,18 +7,40 @@ f = open(inputFile, "r")
 
 input = [x.split("\n") for x in f.read().split("\n\n")]
 
+for puzzleIndex in range(len(input)):
+    for rowIndex in range(len(input[puzzleIndex])):
+        input[puzzleIndex][rowIndex] = [*input[puzzleIndex][rowIndex]]
+
 output = 0
 
 def findDuplicates(arr):
-    print(arr)
+    matchesList = list()
+    # print(arr[0])
+    for rowIndex in range(len(arr) - 1):
+        # print(arr[rowIndex + 1])
+        if "".join(arr[rowIndex]) == "".join(arr[rowIndex + 1]):
+            # print(rowIndex)
+            matchesList.append(rowIndex)
+            # return rowIndex
+    # if matchesList == list():
+    #     # print("No Matches")
+    #     return None
+    # print(matchesList)
+    return matchesList
+
+def validateMirror(arr, index):
+    pass
 
 start = datetime.datetime.now()
 
 for pattern in input:
-    # print(pattern)
     arr = np.array(pattern)
-    findDuplicates(arr)
-    # findDuplicates(np.transpose(arr))
+    patternDict = {
+        "columnIndexes": findDuplicates(np.transpose(arr)),
+        "rowIndexes": findDuplicates(arr) 
+        }
+    print(patternDict)
+
 
 end = datetime.datetime.now() - start
 print("Time: ", end)
