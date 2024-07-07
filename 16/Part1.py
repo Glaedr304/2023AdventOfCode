126734141ed0f4b7067cb34e0cf30cd1adddcd02
Glaedr304 <Davidfrey304@gmail.com>
2024-07-07 10:44:16 -0500

    Day 16 part 1 progress
    I just discovered there are mirror loops, I will have to figure out how to account for these as to not have an endless program

--- 16/Part1.py
+++ 16/Part1.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 output = 0
 
-inputFile = "input.txt"
+inputFile = "input2.txt"
 
 f = open(inputFile, "r")
 
@@ -21,31 +21,77 @@ class Beam:
         self.x: int = x
         self.y: int = y
 
-    def moveBeam(self, Dir: str) -> None: 
-        if   Dir == "N":
-            pass    
-        elif Dir == "E":
-            pass
-        elif Dir == "S":
-            pass
-        elif Dir == "W":
-            pass
-        else:
-            print("fuck, how did you get here?")
-
 beamStack = [
         Beam(-1, 0, "E")
     ]
 
+def moveBeam(Dir: str) -> None: 
+    if   Dir == "N":
+        beamStack[0].y -= 1     
+    elif Dir == "E":
+        beamStack[0].x += 1 
+    elif Dir == "S":
+        beamStack[0].y += 1 
+    elif Dir == "W":
+        beamStack[0].x -= 1 
+    else:
+        print("fuck, how did you get here?")
+    
+    if beamStack[0].x < 0 or beamStack[0].y < 0 or beamStack[0].x >= len(mirrorMap[0]) or beamStack[0].y >= len(mirrorMap):
+        return None
+    energize(beamStack[0].x, beamStack[0].y)
 
+    thisChar = mirrorMap[beamStack[0].x][beamStack[0].y]
+
+    if thisChar == ".":
+        moveBeam(Dir)
+    elif thisChar == "/" or thisChar == "\\":
+        newBeamDir = {
+            "/": {
+                "N":"E",
+                "W":"S",
+                "E":"N",
+                "S":"W"
+            },
+            "\\": {
+                "N":"W",
+                "W":"N",
+                "E":"S",
+                "S":"E"
+            }
+        }
+        beamStack.append(Beam(beamStack[0].x, beamStack[0].y, newBeamDir[thisChar][Dir])) 
+
+    elif thisChar == "|" or thisChar == "-":
+        newBeamDir = {
+            "|": {
+                "N":["N"],
+                "W":["N", "S"],
+                "E":["N", "S"],
+                "S":["S"]
+            },
+            "-": {
+                "N":["E", "W"],
+                "W":["W"],
+                "E":["E"],
+                "S":["E", "W"]
+            }
+        }
+        for newDir in newBeamDir[thisChar]:
+            beamStack.append(Beam(beamStack[0].x, beamStack[0].y, newDir))
+    else:
+        print("I shouldnt be here.")
 
 def energize(xPos: int, yPos: int) -> None:
     energizerMap[yPos*len(mirrorMap) + xPos] = "#"
 
 start = datetime.datetime.now()
 
-for beam in beamStack:
-    beam.moveBeam(beam.Dir)
+while len(beamStack) != 0:
+# for beam in beamStack:
+    moveBeam(beamStack[0].Dir)
+
+output = energizerMap.count("#")
 
 end = datetime.datetime.now() - start
 
