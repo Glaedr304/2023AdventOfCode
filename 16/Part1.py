3141d00b93332c0013a2fcc2b58b572a7dffdcd6
Glaedr304 <Davidfrey304@gmail.com>
2024-07-08 21:50:45 -0500

    Day 16 Part 1 Complete

--- 16/Part1.py
+++ 16/Part1.py
@@ -3,25 +3,26 @@ from pprint import pprint
 
 output = 0
 
-inputFile = "input2.txt"
+inputFile = "input.txt"
 
 f = open(inputFile, "r")
 
-mirrorMap = f.read().split("\n")
-
-pprint(mirrorMap)
+mirrorMap = tuple(f.read().split("\n"))
 
 energizerMap = list()
 for index in range(len(mirrorMap)*len(mirrorMap[0])):
     energizerMap.append(".")
-
-# pprint(mirrorMap)
 
 class Beam:
     def __init__(self, x, y, Dir):
         self.Dir: str = Dir
         self.x: int = x
         self.y: int = y
+        self.char: str | None = None
+    
+    def __str__(self):
+        return f"X:{self.x + 1}, Y:{self.y + 1}, Dir:{self.Dir}"
+
 
 beamStack = [
         Beam(-1, 0, "E")
@@ -39,7 +40,6 @@ def moveBeam() -> None:
     else:
         print("fuck, how did you get here?")
     
-    # print("\r", beamStack[0].x, beamStack[0].y, end = "")
     if beamStack[0].x < 0 or beamStack[0].y < 0 or beamStack[0].x >= len(mirrorMap[0]) or beamStack[0].y >= len(mirrorMap):
         beamStack.pop(0)
         return None
@@ -47,7 +47,6 @@ def moveBeam() -> None:
         energize(beamStack[0].x, beamStack[0].y)
 
     thisChar = mirrorMap[beamStack[0].y][beamStack[0].x]
-    print("This character is", thisChar, "at", beamStack[0].x, beamStack[0].y)
 
     if thisChar == ".":
         moveBeam()
@@ -66,9 +65,7 @@ def moveBeam() -> None:
                 "S":"E"
             }
         }
-        print("Before:", beamStack[0].x, ":", beamStack[0].y, ",", beamStack[0].Dir)
         beamStack[0].Dir = newBeamDir[thisChar][beamStack[0].Dir]
-        print("After:", beamStack[0].x, ":", beamStack[0].y, ",", beamStack[0].Dir)
         moveBeam()
     elif thisChar == "|" or thisChar == "-":
         newBeamDir = {
@@ -85,32 +82,30 @@ def moveBeam() -> None:
                 "S":["E", "W"]
             }
         }
-        print("Before:", beamStack[0].x, ":", beamStack[0].y, ",", beamStack[0].Dir)
-        if len(newBeamDir[thisChar]) == 1:
+        if len(newBeamDir[thisChar][beamStack[0].Dir]) == 1:
             moveBeam()
         else:
-            for newDir in newBeamDir[thisChar]:
+            for newDir in newBeamDir[thisChar][beamStack[0].Dir]:
                 beamStack.append(Beam(beamStack[0].x, beamStack[0].y, newDir))
-                beamStack.pop(0)
+            beamStack.pop(0)
     else:
         print("I shouldnt be here.")
 
 def energize(xPos: int, yPos: int) -> None:
-    print("Energizing!")
     energizerMap[yPos*len(mirrorMap) + xPos] = "#"
 
 start = datetime.datetime.now()
 
 while len(beamStack) != 0:
-# for beam in beamStack:
-    print("This Beam:", beamStack[0].x, beamStack[0].y, beamStack[0].Dir)
     moveBeam()
-    # print("\r", beamStack[0].x, beamStack[0].y, len(beamStack), end="")
 
-output = energizerMap.count("#")
+    output = energizerMap.count("#")
+    print("\r", output, end="")
 
 end = datetime.datetime.now() - start
 
 print("Time: ", end)
 
 print("Output: ", output)
+
+# This works but its stupid, i basically waited until the output was steady, not until the script ended, it would never end.
